dae2099b4753a76947227ec061a3c22dbe4eb2fa
Bryan <bryan@itsthedevman.com>
2025-01-23 01:18:55 -0500

    Welcome back thor - needed the helpers

--- lib/spec_forge.rb
+++ lib/spec_forge.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 require "commander"
+require "thor"
 
 require_relative "spec_forge/version"
 require_relative "spec_forge/cli"
 
 module SpecForge
   class Error < StandardError; end
+
+  def self.root
+    Pathname.pwd
+  end
 end
