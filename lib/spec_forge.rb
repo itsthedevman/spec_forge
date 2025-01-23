a8a991c25dcbd472a5fd975e96aa223b05948618
Bryan <bryan@itsthedevman.com>
2025-01-22 23:21:00 -0500

    Initial

---
+++ lib/spec_forge.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spec_forge/version"
+
+module SpecForge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "spec_forge"
+
+require "minitest/autorun"
