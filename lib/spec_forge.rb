d06b6b331c4657736b758a86eba51ef30f77d2ce
Bryan <bryan@itsthedevman.com>
2025-01-22 23:27:56 -0500

    Cleanup

--- lib/spec_forge.rb
+++ lib/spec_forge.rb
@@ -4,5 +4,4 @@ require_relative "spec_forge/version"
 
 module SpecForge
   class Error < StandardError; end
-  # Your code goes here...
 end
--- test/test_spec_forge.rb
+++ test/test_spec_forge.rb
@@ -6,8 +6,4 @@ class TestSpecForge < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::SpecForge::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
