f7628d6f1e5e9a5df962ec8f3b69bbaecf4f0fef
Bryan <bryan@itsthedevman.com>
2025-01-28 11:46:34 -0500

    Initial

---
+++ spec/lib/spec_forge/attribute/attribute_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe SpecForge::Attribute do
+  describe ".from" do
+    context "when the input is a String" do
+      context "and it is the faker macro"
+      # context "and it is the transform macro"
+      # context "and it is the variable macro"
+      # context "and it is the factory macro"
+      context "and it is anything else"
+    end
+
+    context "when the input is a Hash"
+    context "when the input is a Boolean"
+    context "when the input is an Array"
+  end
+end
