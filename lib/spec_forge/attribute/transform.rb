20c42ae3e78fce2daf60e3ede48ab6f3dcfb4d0c
Bryan <bryan@itsthedevman.com>
2025-01-28 20:58:46 -0500

    Added transform attribute

---
+++ lib/spec_forge/attribute/transform.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module SpecForge
+  class Attribute
+    class Transform < Parameterized
+      attr_reader :transform_method
+
+      def initialize(...)
+        super
+
+        # Remove prefix
+        function = @input.sub("transform.", "")
+
+        @transform_method =
+          case function
+          when "join"
+            lambda do
+              # Technically supports any attribute, but I ain't gonna test all them edge cases
+              array = @arguments[:positional].map { |i| Attribute.from(i).value }
+              array.join
+            end
+          else
+            raise InvalidTransformFunction, @input
+          end
+      end
+
+      def value
+        @transform_method.call
+      end
+    end
+  end
+end
---
+++ spec/lib/spec_forge/attribute/transform_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+RSpec.describe SpecForge::Attribute::Transform do
+  let(:function) { "" }
+  let(:positional) { [] }
+  let(:keyword) { {} }
+
+  subject(:attribute) { described_class.new(function, positional, keyword) }
+
+  context "when the function is not defined" do
+    it "is expected to raise" do
+      expect { attribute }.to raise_error(SpecForge::InvalidTransformFunction)
+    end
+  end
+
+  describe "transform.join" do
+    let(:function) { "transform.join" }
+
+    context "when the joining items are literals" do
+      let(:positional) { ["foo", " ", "bar"] }
+
+      it "joins them into one string" do
+        expect(attribute.value).to eq("foo bar")
+      end
+    end
+
+    context "when there are macros" do
+      let(:positional) { ["faker.number.positive", " ", "bar"] }
+
+      it "replaces the macro and joins them into one string" do
+        expect(attribute.value).to match(/\d+ bar/i)
+      end
+    end
+
+    # It made me so happy when I realized this would work :D
+    context "when there are expanded macros" do
+      let(:positional) { [{"faker.number.between": {from: 1, to: 10}}, " ", "bar"] }
+
+      it "replaces the macro and joins them into one string" do
+        expect(attribute.value).to match(/\d+ bar/i)
+      end
+    end
+  end
+end
