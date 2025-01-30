66c62fb83712008981f11f6c782803c73cd66763
Bryan <bryan@itsthedevman.com>
2025-01-30 15:06:26 -0500

    Added Request specs

---
+++ spec/lib/spec_forge/request_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+RSpec.describe SpecForge::Request do
+  let(:path) { "/users" }
+  let(:method) {}
+  let(:content_type) {}
+  let(:params) {}
+  let(:body) {}
+
+  subject(:request) do
+    described_class.new(path:, method:, content_type:, params:, body:)
+  end
+
+  describe "#initialize" do
+    it "defaults content_type to application/json" do
+      expect(request.content_type).to be_kind_of(MIME::Type)
+      expect(request.content_type).to eq("application/json")
+    end
+
+    it "defaults http_method to GET" do
+      expect(request.http_method).to be_kind_of(SpecForge::HTTPMethod::Get)
+    end
+
+    context "when params are provided" do
+      let(:params) { {id: Faker::String.random, filter: "faker.string.random"} }
+
+      it "is expected to convert them to Attribute" do
+        expect(request.params).to match(
+          hash_including(
+            id: be_kind_of(SpecForge::Attribute::Literal),
+            filter: be_kind_of(SpecForge::Attribute::Faker)
+          )
+        )
+      end
+    end
+
+    context "when 'content_type' is not a valid MIME type" do
+      let(:content_type) { "in/valid" }
+
+      it { expect { request }.to raise_error(ArgumentError) }
+    end
+
+    context "when 'params' is not a Hash" do
+      let(:params) { "raw_params=are%20not%20allowed" }
+
+      it { expect { request }.to raise_error(TypeError, "Expected Hash, got String for 'params'") }
+    end
+
+    context "when 'content_type' is json but 'body' is not a Hash" do
+      # Default content_type is already json
+      let(:body) { "not_a_hash" }
+
+      it { expect { request }.to raise_error(TypeError, "Expected Hash, got String for 'body'") }
+    end
+
+    context "when 'http_method' is mixed case" do
+      let(:method) { "DeLeTe" }
+
+      it "works because it is case insensitive" do
+        expect(request.http_method).to eq("DELETE")
+      end
+    end
+  end
+
+  describe "#with" do
+    subject(:updated_request) { request.with(content_type: "text/plain", method: "POST") }
+
+    it "converts the new attributes correctly" do
+      expect(updated_request.content_type).to eq("text/plain")
+      expect(updated_request.http_method).to eq("POST")
+    end
+  end
+end
--- spec/lib/spec_forge/spec_spec.rb
+++ spec/lib/spec_forge/spec_spec.rb
@@ -18,55 +18,6 @@ RSpec.describe SpecForge::Spec do
       it "is valid" do
         expect(spec).to be_kind_of(described_class)
       end
-
-      it "defaults content_type to application/json" do
-        expect(spec.content_type).to be_kind_of(MIME::Type)
-        expect(spec.content_type).to eq("application/json")
-      end
-
-      it "defaults http_method to GET" do
-        expect(spec.http_method).to be_kind_of(SpecForge::HTTPMethod::Get)
-      end
-    end
-
-    context "when params are provided" do
-      let(:params) { {id: Faker::String.random, filter: "faker.string.random"} }
-
-      it "is expected to convert them to Attribute" do
-        expect(spec.params).to match(
-          hash_including(
-            id: be_kind_of(SpecForge::Attribute::Literal),
-            filter: be_kind_of(SpecForge::Attribute::Faker)
-          )
-        )
-      end
-    end
-
-    context "when 'content_type' is not a valid MIME type" do
-      let(:content_type) { "in/valid" }
-
-      it { expect { spec }.to raise_error(ArgumentError) }
-    end
-
-    context "when 'params' is not a Hash" do
-      let(:params) { "raw_params=are%20not%20allowed" }
-
-      it { expect { spec }.to raise_error(TypeError, "Expected Hash, got String for 'params'") }
-    end
-
-    context "when 'content_type' is json but 'body' is not a Hash" do
-      # Default content_type is already json
-      let(:body) { "not_a_hash" }
-
-      it { expect { spec }.to raise_error(TypeError, "Expected Hash, got String for 'body'") }
-    end
-
-    context "when 'http_method' is mixed case" do
-      let(:method) { "DeLeTe" }
-
-      it "works because it is case insensitive" do
-        expect(spec.http_method).to eq("DELETE")
-      end
     end
 
     context "when 'expectations' are given" do
