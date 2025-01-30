6433b7f47b5ad71fcb38fe0a928c44482308d973
Bryan <bryan@itsthedevman.com>
2025-01-30 00:24:38 -0500

    Added compile method to Expectation

--- lib/spec_forge/spec/expectation.rb
+++ lib/spec_forge/spec/expectation.rb
@@ -3,10 +3,38 @@
 module SpecForge
   class Spec
     class Expectation
-      attr_reader :input, :status
+      attr_reader :input, :spec, :status, :variables, :json # :xml, :html
 
       def initialize(input)
         @input = input
+      end
+
+      def compile(spec)
+        @spec = spec
+
+        # Only hash is supported
+        raise TypeError, "Expected Hash, got #{input.class}" if !input.is_a?(Hash)
+
+        # Status is the only required field
+        @status = input[:status]
+        raise "Missing 'status'" if @status.blank?
+
+        # Check for variables (optional) and convert
+        @variables = input[:variables] || {}
+        raise TypeError, "Expected Hash, got #{variables.class}" if !variables.is_a?(Hash)
+
+        @variables.transform_values! { |v| Attribute.from(v) }
+
+        # Check for json (optional) and convert
+        @json = input[:json] || {}
+        raise TypeError, "Expected Hash, got #{json.class}" if !json.is_a?(Hash)
+
+        @json.transform_values! { |v| Attribute.from(v) }
+
+        # Create a new copy of the request with any overwritten values
+        @request = spec.request.with(**input)
+
+        self
       end
     end
   end
---
+++ spec/lib/spec_forge/spec/expectation_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe SpecForge::Spec::Expectation do
+  describe "#compile" do
+    context "when input is an empty hash"
+    context "when input is a valid hash"
+    context "when input is not a hash"
+    context "when variables is not a hash"
+  end
+end
