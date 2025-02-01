771587eedfa9e92c29303b304b681fb241b9aafa
Bryan <bryan@itsthedevman.com>
2025-02-01 00:59:22 -0500

    Cleaned up Request

--- lib/spec_forge/request.rb
+++ lib/spec_forge/request.rb
@@ -2,48 +2,56 @@
 
 module SpecForge
   class Request < Data.define(:url, :http_method, :content_type, :params, :body)
-    def self.normalize_body(content_type, body)
-      # Body can support different types. Only supporting JSON and plain text right now
-      case content_type
-      when "application/json"
-        if !body.is_a?(Hash)
-          raise InvalidTypeError.new(body, Hash, for: "'body'")
-        end
+    CONTENT_TYPES = {
+      "application/json" => ->(body) { validate_and_transform_hash(body) },
+      "text/plain" => ->(body) { Attribute.from(body.to_s) }
+    }.freeze
+
+    class << self
+      def normalize_body(content_type, body)
+        transformer = CONTENT_TYPES[content_type.to_s]
+        raise ArgumentError, "Unsupported content type: #{content_type}" if transformer.nil?
+
+        transformer.call(body)
+      end
+
+      private
+
+      def validate_and_transform_hash(body)
+        raise InvalidTypeError.new(body, Hash, for: "'body'") unless body.is_a?(Hash)
 
         body.transform_values { |v| Attribute.from(v) }
-      when "text/plain"
-        Attribute.from(body.to_s)
       end
     end
 
+    #
+    # Initializes a new Request instance with the given options
+    #
+    # @param [Hash] options The options to create the Request with
+    #
+    # @option options [String] :path The request path (alias for :url)
+    # @option options [String] :url The request URL
+    #
+    # @option options [String, HTTPMethod] :method The HTTP method to use (alias for :http_method)
+    # @option options [String, HTTPMethod] :http_method The HTTP method to use
+    #
+    # @option options [String] :content_type
+    #  The content type for the request (defaults to "application/json")
+    #
+    # @option options [Hash] :params The query parameters for the request (defaults to {})
+    # @option options [Hash, String] :body The request body (defaults to {})
+    #
     def initialize(**options)
-      url = options[:path] || options[:url]
-
-      http_method = options[:method] || options[:http_method] || "GET"
-      http_method =
-        if http_method.is_a?(String)
-          HTTPMethod.from(http_method)
-        else
-          http_method
-        end
-
-      content_type = options[:content_type] || "application/json"
-      content_type = MIME::Types[content_type].first
-
-      if content_type.nil?
-        raise ArgumentError, "Invalid 'content_type' provided: #{content_type.inspect}"
-      end
-
-      # Params can only be a hash
-      params = options[:params] || {}
-      if !params.is_a?(Hash)
-        raise InvalidTypeError.new(params, Hash, for: "'params'")
-      end
-
-      params = params.transform_values { |v| Attribute.from(v) }
-      body = self.class.normalize_body(content_type, options[:body] || {})
-
-      super(url:, http_method:, content_type:, params:, body:)
+      super(
+        url: extract_url(options),
+        http_method: normalize_http_method(options),
+        content_type: normalize_content_type(options),
+        params: normalize_params(options[:params] || {}),
+        body: self.class.normalize_body(
+          options[:content_type] || "application/json",
+          options[:body] || {}
+        )
+      )
     end
 
     def update(body, params)
@@ -51,6 +59,36 @@ module SpecForge
         body: self.body.merge(body),
         params: self.params.merge(params)
       )
+    end
+
+    private
+
+    def extract_url(options)
+      options[:path] || options[:url]
+    end
+
+    def normalize_http_method(options)
+      method = options[:method] || options[:http_method] || "GET"
+
+      if method.is_a?(String)
+        HTTPMethod.from(method)
+      else
+        method
+      end
+    end
+
+    def normalize_content_type(options)
+      type = options[:content_type] || "application/json"
+      mime_type = MIME::Types[type].first
+      return mime_type if mime_type
+
+      raise ArgumentError, "Invalid content type: #{type.inspect}"
+    end
+
+    def normalize_params(params)
+      raise InvalidTypeError.new(params, Hash, for: "'params'") unless params.is_a?(Hash)
+
+      params.transform_values { |v| Attribute.from(v) }
     end
   end
 end
