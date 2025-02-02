852f527ec9da6f860d75def35ec7dc29e5292bcc
Bryan <bryan@itsthedevman.com>
2025-02-01 23:45:25 -0500

    Changed how literal displays

--- lib/spec_forge/attribute/literal.rb
+++ lib/spec_forge/attribute/literal.rb
@@ -3,13 +3,20 @@
 module SpecForge
   class Attribute
     class Literal < Attribute
-      def value
-        case input
-        when Array
-          input.map { |v| Attribute.from(v) }
-        else
-          input
-        end
+      attr_reader :value
+
+      def initialize(input)
+        super
+
+        @value =
+          case input
+          when Array
+            input.map { |v| Attribute.from(v) }
+          when Hash
+            input.transform_values { |v| Attribute.from(v) }
+          else
+            input
+          end
       end
     end
   end
