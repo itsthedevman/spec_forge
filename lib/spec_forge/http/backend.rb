ec7fbf6025acbf3f71c27c1c77a2039b68bbd60b
Bryan <bryan@itsthedevman.com>
2025-02-02 23:09:30 -0500

    Implemented backend

--- lib/spec_forge/http/backend.rb
+++ lib/spec_forge/http/backend.rb
@@ -3,35 +3,52 @@
 module SpecForge
   module HTTP
     class Backend
+      attr_reader :connection
+
       def initialize(request)
-        @connection = Faraday.new(
-          url: request.base_url,
-          params: request.query.resolve,
-          headers: {
-            "Content-Type" => request.content_type.to_s,
-            request.authorization[:header] => request.authorization[:value]
-          }
-        )
+        @connection =
+          Faraday.new(url: request.base_url) do |builder|
+            # Authorization
+            builder.headers[request.authorization[:header]] = request.authorization[:value]
+
+            # Content-Type
+            if request.content_type == "application/json"
+              builder.request :json
+              builder.response :json
+            else
+              builder.headers["Content-Type"] = request.content_type.to_s
+            end
+
+            # Params
+            builder.params.merge!(request.query.resolve)
+          end
       end
 
       def delete(url, query: {}, body: {})
-        raise "not implemented"
+        connection.delete(url) { |request| update_request(request, query, body) }
       end
 
       def get(url, query: {}, body: {})
-        raise "not implemented"
+        connection.get(url) { |request| update_request(request, query, body) }
       end
 
       def patch(url, query: {}, body: {})
-        raise "not implemented"
+        connection.patch(url) { |request| update_request(request, query, body) }
       end
 
       def post(url, query: {}, body: {})
-        raise "not implemented"
+        connection.post(url) { |request| update_request(request, query, body) }
       end
 
       def put(url, query: {}, body: {})
-        raise "not implemented"
+        connection.put(url) { |request| update_request(request, query, body) }
+      end
+
+      private
+
+      def update_request(request, query, body)
+        request.params.merge!(query)
+        request.body = body.to_json
       end
     end
   end
