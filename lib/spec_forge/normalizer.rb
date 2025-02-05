b0d2130a4b0eb4ea9dbeac624f11a9a8ba250659
Bryan <bryan@itsthedevman.com>
2025-02-05 00:12:30 -0500

    Added default values

--- lib/spec_forge/normalizer.rb
+++ lib/spec_forge/normalizer.rb
@@ -79,104 +79,147 @@ module SpecForge
       STRUCTURE = Normalizer::CONSTRAINT_STRUCTURE
     end
 
-    #
-    # Normalizes a hash by standardizing its keys while ensuring the required data
-    # is provided or defaulted.
-    # Raises InvalidStructureError if anything is missing/invalid type
-    #
-    # @param input [Hash] The hash to normalize
-    #
-    # @return [Hash] A normalized hash as a new instance
-    #
-    def self.normalize(input)
-      errors = []
-
-      begin
-        output, spec_errors = normalize_spec(input)
-        errors += spec_errors if spec_errors.size > 0
-
-        if (expectations = input[:expectations]) && expectations.is_a?(Array)
-          expectation_output, expectation_errors = normalize_expectations(expectations)
-
-          output[:expectations] = expectation_output
-          errors += expectation_errors if expectation_errors.size > 0
-        end
-      rescue => e
-        errors << e
+    class << self
+      #
+      # Generates an empty spec hash
+      #
+      # @return [Hash]
+      #
+      def default_spec
+        generate_default_from_structure(SPEC_STRUCTURE)
       end
 
-      raise InvalidStructureError.new(errors) if errors.size > 0
-
-      output
-    end
-
-    #
-    # Normalize a spec hash
-    # Used internally by .normalize, but is available for utility
-    #
-    # @param spec [Hash] Spec representation as a Hash
-    #
-    # @return [Array] Two item array
-    #   First - The normalized hash
-    #   Second - Array of errors, if any
-    #
-    def self.normalize_spec(spec)
-      raise InvalidTypeError.new(spec, Hash, for: "spec") if !spec.is_a?(Hash)
-
-      Normalizer::Spec.new("spec", spec).normalize
-    end
-
-    #
-    # Normalize an array of expectation hashes
-    # Used internally by .normalize, but is available for utility
-    #
-    # @param spec [Array<Hash>] An array of expectation hashes
-    #
-    # @return [Array] Two item array
-    #   First - The normalized Array<Hash>
-    #   Second - Array of errors, if any
-    #
-    def self.normalize_expectations(expectations)
-      if !expectations.is_a?(Array)
-        raise InvalidTypeError.new(expectations, Array, for: "\"expectations\" on spec")
+      #
+      # Generates an empty expectation hash
+      #
+      # @return [Hash]
+      #
+      def default_expectation
+        generate_default_from_structure(EXPECTATION_STRUCTURE)
       end
 
-      final_errors = []
-      final_output = expectations.map.with_index do |expectation, index|
-        normalizer = Normalizer::Expectation.new("expectation (item #{index})", expectation)
-        output, errors = normalizer.normalize
+      #
+      # Generates an empty constraint hash
+      #
+      # @return [Hash]
+      #
+      def default_constraint
+        generate_default_from_structure(CONSTRAINT_STRUCTURE)
+      end
 
-        # If expect is not provided, skip the constraints
-        if (constraint = expectation[:expect])
-          constraint_output, constraint_errors = Normalizer::Constraint.new(
-            "expect (item #{index})", constraint
-          ).normalize
+      #
+      # Normalizes a hash by standardizing its keys while ensuring the required data
+      # is provided or defaulted.
+      # Raises InvalidStructureError if anything is missing/invalid type
+      #
+      # @param input [Hash] The hash to normalize
+      #
+      # @return [Hash] A normalized hash as a new instance
+      #
+      def normalize(input)
+        errors = []
 
-          output[:expect] = constraint_output
-          errors += constraint_errors if constraint_errors.size > 0
+        begin
+          output, spec_errors = normalize_spec(input)
+          errors += spec_errors if spec_errors.size > 0
+
+          if (expectations = input[:expectations]) && expectations.is_a?(Array)
+            expectation_output, expectation_errors = normalize_expectations(expectations)
+
+            output[:expectations] = expectation_output
+            errors += expectation_errors if expectation_errors.size > 0
+          end
+        rescue => e
+          errors << e
         end
 
-        final_errors += errors if errors.size > 0
+        raise InvalidStructureError.new(errors) if errors.size > 0
+
         output
       end
 
-      [final_output, final_errors]
-    end
+      #
+      # Normalize a spec hash
+      # Used internally by .normalize, but is available for utility
+      #
+      # @param spec [Hash] Spec representation as a Hash
+      #
+      # @return [Array] Two item array
+      #   First - The normalized hash
+      #   Second - Array of errors, if any
+      #
+      def normalize_spec(spec)
+        raise InvalidTypeError.new(spec, Hash, for: "spec") if !spec.is_a?(Hash)
 
-    #
-    # Normalize a constraint hash
-    # Used internally by .normalize, but is available for utility
-    #
-    # @param spec [Hash] Spec representation as a Hash
-    #
-    # @return [Array] Two item array
-    #   First - The normalized hash
-    #   Second - Array of errors, if any
-    #
-    def self.normalize_constraint(constraint)
-      raise InvalidTypeError.new(constraint, Hash, for: "expect") if !constraint.is_a?(Hash)
+        Normalizer::Spec.new("spec", spec).normalize
+      end
 
-      Normalizer::Constraint.new("expect", constraint).normalize
+      #
+      # Normalize an array of expectation hashes
+      # Used internally by .normalize, but is available for utility
+      #
+      # @param spec [Array<Hash>] An array of expectation hashes
+      #
+      # @return [Array] Two item array
+      #   First - The normalized Array<Hash>
+      #   Second - Array of errors, if any
+      #
+      def normalize_expectations(expectations)
+        if !expectations.is_a?(Array)
+          raise InvalidTypeError.new(expectations, Array, for: "\"expectations\" on spec")
+        end
+
+        final_errors = []
+        final_output = expectations.map.with_index do |expectation, index|
+          normalizer = Normalizer::Expectation.new("expectation (item #{index})", expectation)
+          output, errors = normalizer.normalize
+
+          # If expect is not provided, skip the constraints
+          if (constraint = expectation[:expect])
+            constraint_output, constraint_errors = Normalizer::Constraint.new(
+              "expect (item #{index})", constraint
+            ).normalize
+
+            output[:expect] = constraint_output
+            errors += constraint_errors if constraint_errors.size > 0
+          end
+
+          final_errors += errors if errors.size > 0
+          output
+        end
+
+        [final_output, final_errors]
+      end
+
+      #
+      # Normalize a constraint hash
+      # Used internally by .normalize, but is available for utility
+      #
+      # @param spec [Hash] Spec representation as a Hash
+      #
+      # @return [Array] Two item array
+      #   First - The normalized hash
+      #   Second - Array of errors, if any
+      #
+      def normalize_constraint(constraint)
+        raise InvalidTypeError.new(constraint, Hash, for: "expect") if !constraint.is_a?(Hash)
+
+        Normalizer::Constraint.new("expect", constraint).normalize
+      end
+
+      private
+
+      def generate_default_from_structure(structure)
+        structure.transform_values do |value|
+          if (default = value[:default])
+            default.dup
+          elsif value[:type] == Integer # Can't call new on int
+            0
+          else
+            value[:type].new
+          end
+        end
+      end
     end
 
     attr_reader :label, :input, :structure
