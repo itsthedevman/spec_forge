3bda6e691396cabe2e48fd5a4aa8a480b54dc2aa
Bryan <bryan@itsthedevman.com>
2025-02-07 22:32:08 -0500

    Moved over to Data

--- lib/spec_forge/config.rb
+++ lib/spec_forge/config.rb
@@ -1,43 +1,24 @@
 # frozen_string_literal: true
 
 module SpecForge
-  class Config < Struct.new(:base_url, :authorization, :factories)
+  class Config < Data.define(:base_url, :authorization, :factories)
     include Singleton
 
     def initialize
-      load_defaults
-      load_from_file
+      config = Normalizer.default_config.deep_merge(load_from_file)
+      normalized = Normalizer.normalize_config!(config)
+
+      super(**normalized)
     end
 
     private
-
-    def load_from_hash(hash)
-      if (config = hash[:base_url]) && config.present?
-        self.base_url = config
-      end
-
-      if (config = hash[:authorization]) && config.present?
-        self.authorization = config.to_struct
-      end
-
-      if (config = hash[:authorization]) && config.present?
-        self.factories = config.to_struct
-      end
-    end
-
-    def load_defaults
-      defaults = Normalizer.default_config
-      load_from_hash(defaults)
-    end
 
     def load_from_file
       path = SpecForge.forge.join("config.yml")
       return unless File.exist?(path)
 
       erb = ERB.new(File.read(path)).result
-      hash = YAML.safe_load(erb, aliases: true, symbolize_names: true)
-
-      load_from_hash(hash)
+      YAML.safe_load(erb, aliases: true, symbolize_names: true)
     end
   end
 end
