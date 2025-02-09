04abd68e31e5601dc03e7fc8bcaf34e7437d9d46
Bryan <bryan@itsthedevman.com>
2025-02-08 21:11:45 -0500

    Normalizer::FactoryReference

--- lib/spec_forge/normalizer/factory_reference.rb
+++ lib/spec_forge/normalizer/factory_reference.rb
@@ -2,8 +2,63 @@
 
 module SpecForge
   class Normalizer
+    class FactoryReference < Normalizer
+      STRUCTURE = {
+        attributes: {
+          type: Hash,
+          default: {}
+        },
+        build_strategy: {
+          type: String,
+          aliases: %i[strategy],
+          default: "create"
+        }
+      }.freeze
+    end
+
     # On Normalizer
     class << self
+      #
+      # Generates an empty Attribute::Factory hash
+      #
+      # @return [Hash]
+      #
+      def default_factory_reference
+        FactoryReference.default
+      end
+
+      #
+      # Normalizes a Attribute::Factory hash by standardizing
+      # its keys while ensuring the required data is provided or defaulted.
+      # Raises InvalidStructureError if anything is missing/invalid type
+      #
+      # @param input [Hash] The hash to normalize
+      #
+      # @return [Hash] A normalized hash as a new instance
+      #
+      def normalize_factory_reference!(input, **)
+        raise_errors! do
+          normalize_factory_reference(input, **)
+        end
+      end
+
+      #
+      # Normalize a factory hash
+      # Used internally by .normalize_factory_reference, but is available for utility
+      #
+      # @param factory [Hash] Attribute::Factory representation as a Hash
+      #
+      # @return [Array] Two item array
+      #   First - The normalized hash
+      #   Second - Array of errors, if any
+      #
+      # @private
+      #
+      def normalize_factory_reference(factory, label: "factory reference")
+        raise InvalidTypeError.new(factory, Hash, for: "factory reference") if !factory.is_a?(Hash)
+
+        Normalizer::FactoryReference.new(label, factory).normalize
+      end
     end
   end
 end
---
+++ spec/lib/spec_forge/normalizer/factory_reference_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+# This is more for organizational purposes
+RSpec.describe SpecForge::Normalizer do
+  describe ".normalize_factory_reference!" do
+    let(:factory) do
+      {
+        build_strategy: "create",
+        attributes: {
+          name: "faker.name.name"
+        }
+      }
+    end
+
+    subject(:normalized) { described_class.normalize_factory_reference!(factory) }
+
+    it "is expected to normalize normally" do
+      expect(normalized[:build_strategy]).to eq("create")
+      expect(normalized[:attributes]).to match(name: "faker.name.name")
+    end
+
+    context "when 'build_strategy' is nil" do
+      before do
+        factory[:build_strategy] = nil
+      end
+
+      it do
+        expect(normalized[:build_strategy]).to eq("create")
+      end
+    end
+
+    context "when 'build_strategy' is not a String" do
+      before do
+        factory[:build_strategy] = 1
+      end
+
+      it do
+        expect { normalized }.to raise_error(
+          SpecForge::InvalidStructureError,
+          "Expected String, got Integer for \"build_strategy\" on factory reference"
+        )
+      end
+    end
+
+    context "when 'attributes' is nil" do
+      before do
+        factory[:attributes] = nil
+      end
+
+      it do
+        expect(normalized[:attributes]).to eq({})
+      end
+    end
+
+    context "when 'attributes' is not a Hash" do
+      before do
+        factory[:attributes] = 1
+      end
+
+      it do
+        expect { normalized }.to raise_error(
+          SpecForge::InvalidStructureError,
+          "Expected Hash, got Integer for \"attributes\" on factory reference"
+        )
+      end
+    end
+  end
+end
