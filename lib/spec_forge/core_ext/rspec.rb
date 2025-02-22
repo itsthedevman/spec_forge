3feef22c2a7f0af58c31abdb4b7af1e8f357cb55
Bryan <bryan@itsthedevman.com>
2025-02-22 01:32:27 -0500

    Added core_ext, support for spec_forge commands in error

---
+++ lib/spec_forge/core_ext/rspec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module RSpec
+  module Core
+    module Notifications
+      #
+      # I did attempt to do this without monkey patching
+      # Getting around the `rspec` word was making it difficult
+      #
+      class SummaryNotification
+        # Customizes RSpec's failure output to:
+        # 1. Use 'spec_forge' instead of 'rspec' for rerun commands
+        # 2. Remove line numbers since SpecForge uses dynamic spec generation
+        alias_method :og_colorized_rerun_commands, :colorized_rerun_commands
+
+        def colorized_rerun_commands(colorizer)
+          og_colorized_rerun_commands.sub(/rspec/i, "spec_forge")
+            .sub(/\[[\d:]+\]/, "") # Removes the [1:1:1] at the end
+        end
+      end
+    end
+  end
+end
