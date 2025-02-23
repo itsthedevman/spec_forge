311c20d612289ebc4811d66427a707d6ca36a429
Bryan <bryan@itsthedevman.com>
2025-02-22 20:26:49 -0500

    Set example metadata for error reporting

--- lib/spec_forge/core_ext/rspec.rb
+++ lib/spec_forge/core_ext/rspec.rb
@@ -14,6 +14,18 @@ module RSpec
         alias_method :og_colorized_rerun_commands, :colorized_rerun_commands
 
         def colorized_rerun_commands(colorizer)
+          # Updating these at this point fixes the re-run for some failures - it depends
+          failed_examples.each do |example|
+            metadata = example.metadata[:example_group]
+
+            # I might've uncovered an inconsistency here
+            # When multiple specs fail, it appears that the rerun_commands will use
+            # :rerun_file_path from the example's metadata.
+            # But when a single spec is ran and fails, it's using :location.
+            example.metadata[:location] = metadata[:rerun_file_path]
+            example.metadata[:line_number] = metadata[:line_number]
+          end
+
           og_colorized_rerun_commands.gsub(/rspec/i, "spec_forge")
             .gsub(/\[[\d:]+\]/, "")
         end
--- lib/spec_forge/runner.rb
+++ lib/spec_forge/runner.rb
@@ -23,16 +23,20 @@ module SpecForge
           spec_forge.expectations.each do |expectation|
             # Define the example group
             describe(expectation.name) do
-              # Set up the metadata for reporting
-              runner_forge.setup_example_metadata(self, spec_forge, expectation)
+              # Set up the class metadata for error reporting
+              runner_forge.set_group_metadata(self, spec_forge, expectation)
 
               constraints = expectation.constraints
+
               let!(:expected_status) { constraints.status.resolve }
               let!(:expected_json) { constraints.json.resolve.deep_stringify_keys }
 
               before do
                 # Ensure all variables are called and resolved, in case they are not referenced
                 expectation.variables.resolve
+
+                # Set up the example metadata for error reporting
+                runner_forge.set_example_metadata(spec_forge, expectation)
               end
 
               subject(:response) { expectation.http_client.call }
@@ -56,20 +60,30 @@ module SpecForge
         end
       end
 
+      # @private
       def handle_debug(...)
         DebugProxy.new(...).call
       end
 
-      # Needed for error reporting
-      def setup_example_metadata(context, spec, expectation)
+      # @private
+      def set_group_metadata(context, spec, expectation)
         metadata = {
+          file_path: spec.file_path,
+          absolute_file_path: spec.file_path,
           line_number: spec.line_number,
           location: spec.file_path,
           rerun_file_path: "#{spec.file_name}:#{spec.name}:\"#{expectation.name}\""
         }
 
-        context.superclass_metadata.merge!(metadata)
         context.metadata.merge!(metadata)
+      end
+
+      # @private
+      def set_example_metadata(spec, expectation)
+        # This is needed when an error raises in an example
+        metadata = {location: "#{spec.file_path}:#{spec.line_number}"}
+
+        RSpec.current_example.metadata.merge!(metadata)
       end
     end
 
