d4a14ca5867712add5adf2c02845f614199538b5
Bryan <bryan@itsthedevman.com>
2025-03-02 16:06:30 -0500

    Replaced forge paths

--- spec/lib/spec_forge/loader_spec.rb
+++ spec/lib/spec_forge/loader_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe SpecForge::Loader do
-  let(:file_path_1) { SpecForge.forge.join("specs", "spec_1.yml") }
-  let(:file_path_2) { SpecForge.forge.join("specs", "spec_2.yml") }
+  let(:file_path_1) { SpecForge.forge_path.join("specs", "spec_1.yml") }
+  let(:file_path_2) { SpecForge.forge_path.join("specs", "spec_2.yml") }
 
   describe ".load_from_files" do
     subject(:specs) { described_class.load_from_files }
