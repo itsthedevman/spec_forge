751deaa40be4eb2646564c256bc15480c6494ddf
Bryan <bryan@itsthedevman.com>
2025-03-03 00:01:29 -0500

    Flattened spec

--- lib/spec_forge/spec.rb
+++ lib/spec_forge/spec.rb
@@ -3,29 +3,13 @@
 require_relative "spec/expectation"
 
 module SpecForge
-  class Spec
+  class Spec < Data.define(:id, :name, :file_path, :file_name, :line_number, :expectations)
     attr_predicate :debug
 
-    attr_reader :name, :file_path, :file_name, :line_number, :expectations, :options
+    def initialize(id:, name:, file_path:, file_name:, line_number:, debug:, expectations:)
+      expectations = expectations.map { |e| Expectation.new(e) }
 
-    def initialize(name:, file_path:, file_name:, line_number:, **input)
-      @name = name
-      @file_path = file_path
-      @file_name = file_name
-      @line_number = line_number
-
-      # Don't pass this down to the expectations
-      @debug = input.delete(:debug) || false
-      @options = normalize_options(input)
-
-      @expectations = input[:expectations].map { |e| Expectation.new(e) }
-    end
-
-    private
-
-    def normalize_options(input)
-      config = SpecForge.configuration.to_h.slice(:base_url, :headers, :query)
-      Configuration.overlay_options(config, input.except(:expectations))
+      super
     end
   end
 end
