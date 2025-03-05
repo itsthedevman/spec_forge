04e406bd82c2e9543d18021636c6981557353c60
Bryan <bryan@itsthedevman.com>
2025-03-04 22:57:19 -0500

    Simplified Spec and Expectation

--- lib/spec_forge/spec.rb
+++ lib/spec_forge/spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require_relative "spec/expectation"
-
 module SpecForge
-  class Spec < Data.define(:id, :name, :file_path, :file_name, :line_number, :expectations)
+  class Spec < Data.define(:id, :name, :file_path, :file_name, :debug, :line_number, :expectations)
     attr_predicate :debug
 
     def initialize(id:, name:, file_path:, file_name:, line_number:, debug:, expectations:)
-      expectations = expectations.map { |e| Expectation.new(e) }
+      expectations = expectations.map { |e| Expectation.new(**e) }
 
       super
     end
   end
 end
+
+require_relative "spec/expectation"
--- lib/spec_forge/spec/expectation.rb
+++ lib/spec_forge/spec/expectation.rb
@@ -1,58 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "expectation/constraint"
-
 module SpecForge
   class Spec
-    class Expectation
+    class Expectation < Data.define(:id, :name, :line_number, :debug, :constraints)
       attr_predicate :debug
 
-      attr_reader :name, :variables, :constraints, :http_client
+      def initialize(id:, name:, line_number:, debug:, expect:)
+        constraints = Constraint.new(**expect)
 
-      def initialize(input)
-        load_constraints(input)
-        load_debug(input)
-        load_variables(input)
-
-        @http_client = HTTP::Client.new(**input.except(:name, :variables, :expect, :debug))
-
-        # Must be after http_client
-        load_name(input)
-      end
-
-      def to_h
-        {
-          name:,
-          debug: debug?,
-          variables: variables.resolve,
-          request: http_client.request.to_h,
-          constraints: constraints.to_h
-        }
-      end
-
-      private
-
-      def load_name(input)
-        # GET /users
-        @name = "#{http_client.request.http_verb.upcase} #{http_client.request.url}"
-
-        # GET /users - Returns a 404
-        if (name = input[:name].presence)
-          @name += " - #{name}"
-        end
-      end
-
-      def load_variables(input)
-        @variables = Attribute.from(input[:variables])
-      end
-
-      def load_debug(input)
-        @debug = Attribute.from(input[:debug])
-      end
-
-      def load_constraints(input)
-        @constraints = Constraint.new(**constraints)
+        super(id:, name:, line_number:, debug:, constraints:)
       end
     end
   end
 end
+
+require_relative "expectation/constraint"
