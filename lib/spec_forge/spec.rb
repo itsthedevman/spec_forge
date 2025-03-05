0d0a5be0e5c21b9bb2d6a1767272abb95577b28f
Bryan <bryan@itsthedevman.com>
2025-03-04 23:49:42 -0500

    Back to poro for you

--- lib/spec_forge/spec.rb
+++ lib/spec_forge/spec.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 module SpecForge
-  class Spec < Data.define(:id, :name, :file_path, :file_name, :debug, :line_number, :expectations)
+  class Spec
     attr_predicate :debug
 
-    def initialize(id:, name:, file_path:, file_name:, line_number:, debug:, expectations:)
-      expectations = expectations.map { |e| Expectation.new(**e) }
+    attr_reader :id, :name, :file_path, :file_name, :debug, :line_number
+    attr_accessor :expectations
 
-      super
+    def initialize(id:, name:, file_path:, file_name:, debug:, line_number:, expectations:)
+      @id = id
+      @name = name
+      @file_path = file_path
+      @file_name = file_name
+      @debug = debug
+      @line_number = line_number
+      @expectations = expectations.map { |e| Expectation.new(**e) }
     end
   end
 end
