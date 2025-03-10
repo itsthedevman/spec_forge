ecb32350185cfe3e9b014b9abcf0727e0e36b633
Bryan <bryan@itsthedevman.com>
2025-03-09 22:27:08 -0400

    Initial store

--- lib/spec_forge/context/store.rb
+++ lib/spec_forge/context/store.rb
@@ -2,9 +2,29 @@
 
 module SpecForge
   class Context
-    # Not used yet
     class Store
+      class Entry < Data.define(:scope, :request, :variables, :response)
+        def initialize(request:, variables:, response:, scope: :file)
+          request = request.to_istruct
+          variables = variables.to_istruct
+          response = response.to_istruct
+
+          super
+        end
+
+        def status = response.status
+
+        def body = response.body
+
+        def headers = response.headers
+      end
+
       def initialize
+        @inner = {}
+      end
+
+      def store(id, **)
+        @inner[id] = Entry.new(**)
       end
     end
   end
