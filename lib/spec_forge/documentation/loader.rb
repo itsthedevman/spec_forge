af5072c16a651b754793173b6fbac7c66c0724d8
Bryan <bryan@itsthedevman.com>
2025-04-04 00:27:00 -0400

    loader

--- lib/spec_forge/documentation/loader.rb
+++ lib/spec_forge/documentation/loader.rb
@@ -5,13 +5,76 @@ module SpecForge
     class Loader
       include Singleton
 
-      def self.load(forges)
-        return [] if forges.blank?
-
-        instance.normalize(forges)
+      def self.load
+        instance
+          .run
+          .normalize
       end
 
-      def normalize(forges)
+      def initialize
+        @callback_name = "__sf_docs_#{SpecForge.generate_id(self)}"
+        @successes = []
+      end
+
+      def run
+        forges = setup
+        Runner.run(forges, exit_on_finish: false)
+
+        self
+      ensure
+        teardown
+      end
+
+      def setup
+        @successes.clear
+
+        forges = Runner.prepare
+
+        forges.each do |forge|
+          forge.global[:callbacks] << {after_each: @callback_name}
+        end
+
+        Callbacks.register(@callback_name) do |context|
+          @successes << context if context.example.execution_result.status == :passed
+        end
+
+        forges
+      end
+
+      def teardown
+        Callbacks.deregister(@callback_name)
+      end
+
+      def normalize
+        object = {endpoints: [], structures: []}
+
+        @successes.each_with_object(object) do |context, hash|
+          hash[:endpoints] << extract_endpoint(context)
+        end
+      end
+
+      private
+
+      def extract_endpoint(context)
+        response = context.response
+
+        request_hash = context.request.to_h
+        response_hash = response.to_hash
+
+        {
+          # Request data
+          base_url: request_hash[:base_url],
+          url: request_hash[:url],
+          http_verb: request_hash[:http_verb],
+          request_body: request_hash[:body],
+          request_headers: request_hash[:headers].merge(response.env.request_headers),
+          request_query: request_hash[:query],
+
+          # Response data
+          response_status: response_hash[:status],
+          response_body: response_hash[:body],
+          response_headers: response_hash[:response_headers]
+        }
       end
     end
   end
