13e52dd9e4b8d51391aabde0fde2a743624a67ff
Bryan <bryan@itsthedevman.com>
2025-04-04 23:28:35 -0400

    initial builder

--- lib/spec_forge/documentation/builder.rb
+++ lib/spec_forge/documentation/builder.rb
@@ -2,7 +2,55 @@
 
 module SpecForge
   module Documentation
-    class Builder
+    class Builder < Hash
+      include Singleton
+
+      def self.build(endpoints: [], structures: [])
+        instance.load(endpoints:, structures:)
+          .group_endpoints
+          .combine_endpoints
+      end
+
+      def initialize
+        @endpoints = []
+        @structures = []
+      end
+
+      def load(endpoints:, structures:)
+        default_proc = ->(hash, key) { hash[key] = {} }
+
+        clear
+
+        self[:info] = {}
+        self[:endpoints] = Hash.new(&default_proc)
+        self[:structures] = Hash.new(&default_proc)
+
+        @endpoints = endpoints
+        @structures = structures
+
+        self
+      end
+
+      def group_endpoints
+        @endpoints.each do |input|
+          # "/users" => {}
+          endpoint_hash = self[:endpoints][input[:url]]
+
+          # "GET" => []
+          (endpoint_hash[input[:http_verb]] ||= []) << input
+        end
+
+        self
+      end
+
+      def combine_endpoints
+        self[:endpoints].each do |url, operations|
+          operations.each do |name, operation|
+          end
+        end
+
+        self
+      end
     end
   end
 end
