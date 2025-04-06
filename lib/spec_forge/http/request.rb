cff7217b169b010cdcb2b7eb2f88bbc12028bde4
Bryan <bryan@itsthedevman.com>
2025-04-05 22:05:44 -0400

    Added content_type to request

--- lib/spec_forge/http/request.rb
+++ lib/spec_forge/http/request.rb
@@ -7,7 +7,15 @@ module SpecForge
     #
     # @return [Array<Symbol>]
     #
-    REQUEST_ATTRIBUTES = [:base_url, :url, :http_verb, :headers, :query, :body].freeze
+    REQUEST_ATTRIBUTES = %i[
+      base_url
+      url
+      http_verb
+      content_type
+      headers
+      query
+      body
+    ].freeze
 
     #
     # Represents an HTTP request configuration
@@ -52,8 +60,9 @@ module SpecForge
         query = Attribute.from(options[:query] || {})
         body = Attribute.from(options[:body] || {})
         headers = normalize_headers(options[:headers] || {})
+        content_type = "application/json"
 
-        super(base_url:, url:, http_verb:, headers:, query:, body:)
+        super(base_url:, url:, http_verb:, content_type:, headers:, query:, body:)
       end
 
       #
