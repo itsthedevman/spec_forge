191eb957ac34aae1c8e4c27f240c2c2a43a9e6a7
Bryan <bryan@itsthedevman.com>
2025-04-08 18:38:08 -0400

    added start of v3

--- lib/spec_forge/documentation/renderers/openapi/v3_0.rb
+++ lib/spec_forge/documentation/renderers/openapi/v3_0.rb
@@ -6,6 +6,17 @@ module SpecForge
       module OpenAPI
         class V3_0 < Base # standard:disable Naming/ClassAndModuleCamelCase
           CURRENT_VERSION = "3.0.3"
+
+          def render
+            output[:openapi] = ""
+            output[:info] = {}
+            output[:servers] = []
+            output[:tags] = []
+            output[:security] = []
+            output[:paths] = {}
+            output[:components] = {}
+            output
+          end
         end
       end
     end
