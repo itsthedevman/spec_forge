965d421b08283ae5ff5ee2dcd833f62825409b0b
Bryan <bryan@itsthedevman.com>
2025-04-10 00:46:02 -0400

    type_to_schema

--- lib/spec_forge/documentation/renderers/openapi/base.rb
+++ lib/spec_forge/documentation/renderers/openapi/base.rb
@@ -8,6 +8,29 @@ module SpecForge
           def self.to_sem_version
             SemVersion.new(CURRENT_VERSION)
           end
+
+          protected
+
+          def type_to_schema(format)
+            case format
+            when "datetime", "time"
+              {type: "string", format: "date-time"}
+            when "int64", "i64"
+              {type: "integer", format: "int64"}
+            when "int32", "i32"
+              {type: "integer", format: "int32"}
+            when "double", "float"
+              {type: "number", format:}
+            when "object"
+              {type: "object", properties: {}}
+            when "array"
+              {type: "array", items: []}
+            when "boolean"
+              {type: "boolean"}
+            else
+              {type: "string", format:}
+            end
+          end
         end
       end
     end
---
+++ spec/lib/spec_forge/documentation/renderers/openapi/base_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+RSpec.describe SpecForge::Documentation::Renderers::OpenAPI::Base do
+  describe "#type_to_schema" do
+    let(:type) {}
+
+    subject(:schema) { described_class.new("").send(:type_to_schema, type) }
+
+    context "when the type is 'datetime'" do
+      let(:type) { "datetime" }
+
+      it "is expected to return a schema with type 'string' and format 'date-time'" do
+        is_expected.to eq({type: "string", format: "date-time"})
+      end
+    end
+
+    context "when the type is 'time'" do
+      let(:type) { "time" }
+
+      it "is expected to return a schema with type 'string' and format 'date-time'" do
+        is_expected.to eq({type: "string", format: "date-time"})
+      end
+    end
+
+    context "when the type is 'int64'" do
+      let(:type) { "int64" }
+
+      it "is expected to return a schema with type 'integer' and format 'int64'" do
+        is_expected.to eq({type: "integer", format: "int64"})
+      end
+    end
+
+    context "when the type is 'i64'" do
+      let(:type) { "i64" }
+
+      it "is expected to return a schema with type 'integer' and format 'int64'" do
+        is_expected.to eq({type: "integer", format: "int64"})
+      end
+    end
+
+    context "when the type is 'int32'" do
+      let(:type) { "int32" }
+
+      it "is expected to return a schema with type 'integer' and format 'int32'" do
+        is_expected.to eq({type: "integer", format: "int32"})
+      end
+    end
+
+    context "when the type is 'i32'" do
+      let(:type) { "i32" }
+
+      it "is expected to return a schema with type 'integer' and format 'int32'" do
+        is_expected.to eq({type: "integer", format: "int32"})
+      end
+    end
+
+    context "when the type is 'double'" do
+      let(:type) { "double" }
+
+      it "is expected to return a schema with type 'number' and format 'double'" do
+        is_expected.to eq({type: "number", format: "double"})
+      end
+    end
+
+    context "when the type is 'float'" do
+      let(:type) { "float" }
+
+      it "is expected to return a schema with type 'number' and format 'float'" do
+        is_expected.to eq({type: "number", format: "float"})
+      end
+    end
+
+    context "when the type is 'boolean'" do
+      let(:type) { "boolean" }
+
+      it "is expected to return a schema with type 'boolean'" do
+        is_expected.to eq({type: "boolean"})
+      end
+    end
+
+    context "when the type is 'array'" do
+      let(:type) { "array" }
+
+      it "is expected to return a schema with type 'array'" do
+        is_expected.to eq({type: "array", items: []})
+      end
+    end
+
+    context "when the type is 'object'" do
+      let(:type) { "object" }
+
+      it "is expected to return a schema with type 'object'" do
+        is_expected.to eq({type: "object", properties: {}})
+      end
+    end
+
+    context "when the type is anything else" do
+      let(:type) { "password" }
+
+      it "is expected to return a schema with type 'string' and the format as the type" do
+        is_expected.to eq({type: "string", format: type})
+      end
+    end
+  end
+end
