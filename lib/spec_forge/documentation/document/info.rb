c67d859b89ab6461554875e850d323a9d7f3234b
Bryan <bryan@itsthedevman.com>
2025-04-09 22:13:43 -0400

    Defaults

--- lib/spec_forge/documentation/document/info.rb
+++ lib/spec_forge/documentation/document/info.rb
@@ -4,7 +4,7 @@ module SpecForge
   module Documentation
     class Document
       class Info < Data.define(:title, :version, :description, :contact, :license)
-        def initialize(title:, version:, description:, contact:, license:)
+        def initialize(title: "", version: "", description: "", contact: {}, license: {})
           contact = contact.to_istruct
           license = license.to_istruct
 
