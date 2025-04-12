5f9029a835b83f5e53dfbc04e7ce1f839e913ef2
Bryan <bryan@itsthedevman.com>
2025-04-12 00:24:31 -0400

    Reworked docs

--- lib/spec_forge/documentation.rb
+++ lib/spec_forge/documentation.rb
@@ -4,17 +4,17 @@ module SpecForge
   module Documentation
     def self.config
       @config ||= begin
-        path = SpecForge.forge_path.join("docs", "config.yml")
+        path = SpecForge.docs_path.join("config.yml")
         hash = YAML.safe_load_file(path).deep_symbolize_keys
 
         Normalizer.normalize_documentation_config!(hash)
       end
     end
 
-    def self.generate(renderer)
+    def self.renderer(renderer_class)
       test_data = Documentation::Loader.extract_from_tests
       document = Documentation::Builder.build(**test_data)
-      renderer.new(document).render
+      renderer_class.new(document)
     end
   end
 end
