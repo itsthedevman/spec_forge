addf5e82a6212ecba17f5da4d467e5a3ca1b0c95
Bryan <bryan@itsthedevman.com>
2025-04-14 02:21:39 -0400

    Initial openapi path normalizer

---
+++ lib/spec_forge/normalizer/openapi_path.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+module SpecForge
+  class Normalizer
+    #
+    # Normalizes OpenAPI path hash structure for SpecForge
+    #
+    # Ensures that the path has the correct structure
+    # and default values for all required settings.
+    #
+    class OpenapiPath < Normalizer
+      parameters = {
+        type: Array,
+        default: [],
+        structure: {
+          type: Hash,
+          default: {},
+          structure: {
+            name: {type: String},
+            description: {type: String, default: nil},
+            required: {type: [true, false], default: nil}
+          }
+        }
+      }
+
+      security = {
+        type: Array,
+        default: [],
+        structure: {
+          type: Hash,
+          default: {}
+        }
+      }
+
+      tags = {
+        type: Array,
+        default: [],
+        structure: {type: String}
+      }
+
+      operation = {
+        type: Hash,
+        default: {},
+        structure: {
+          tags:,
+          security:,
+          parameters:,
+          summary: {type: String, default: nil},
+          description: {type: String, default: nil},
+          responses: {type: Hash, default: nil}
+        }
+      }
+
+      #
+      # Defines the normalized structure for configuration validation
+      #
+      # Specifies validation rules for configuration attributes:
+      # - Enforces specific data types
+      # - Provides default values
+      # - Supports alternative key names
+      #
+      # @return [Hash] Configuration attribute validation rules
+      #
+      STRUCTURE = {
+        tags:,
+        parameters:,
+        security:,
+        get: operation,
+        delete: operation,
+        post: operation,
+        patch: operation,
+        put: operation
+      }.freeze
+
+      default_label "openapi paths"
+
+      define_normalizer_methods(self)
+    end
+  end
+end
