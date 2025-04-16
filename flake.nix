f6ffaeb2948a2b0976081a60a85b74e315ad0bf9
Bryan <bryan@itsthedevman.com>
2025-04-16 01:57:08 -0400

    Realized I should be on an earlier ruby version

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Ruby 3.4.2 development environment";
+  description = "Ruby 3.2 development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -20,7 +20,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (ruby_3_4.override {
+            (ruby_3_2.override {
               jemallocSupport = true;
               docSupport = false;
             })
