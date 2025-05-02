5ffd8c9e0f34a660edcef93640697c290c9d0291
Bryan <bryan@itsthedevman.com>
2025-05-02 00:05:21 -0400

    Spec fixes

--- spec/support/generator.rb
+++ spec/support/generator.rb
@@ -7,19 +7,24 @@ module Generator
     end
 
     def empty_expectation_hash
-      SpecForge::Normalizer.default(:expectation).except(:variables, *empty_request_hash.keys)
+      structure = SpecForge::Normalizer.structures
+        # One word: WEEEEEEEE!
+        # Not joking, just how the data is set up
+        .dig(:spec, :structure, :expectations, :structure, :structure)
+
+      SpecForge::Normalizer.default(structure:, include_optional: true)
+        .except(:variables, *empty_request_hash.keys)
     end
 
     def forge(global: {}, metadata: {}, specs: [])
       specs = specs.map do |spec|
-        default_spec = SpecForge::Normalizer.default(:spec)
+        default_spec = SpecForge::Normalizer.default(:spec, include_optional: true)
         spec = default_spec.deep_merge(spec)
 
         spec[:id] = SecureRandom.uuid if spec[:id].blank?
 
         spec[:expectations].map! do |expectation|
-          default_expectation = SpecForge::Normalizer.default(:expectation)
-          expectation = default_expectation.deep_merge(expectation)
+          expectation = empty_expectation_hash.merge(expectation)
 
           expectation[:id] = SecureRandom.uuid if expectation[:id].blank?
           expectation[:expect][:status] = 404 if expectation[:expect][:status].blank?
