f89570d2dd24ccb36a18eb67dede6763ce9d9892
Bryan <bryan@itsthedevman.com>
2025-05-11 23:41:21 -0400

    Fixed label, removed schema methods

--- lib/spec_forge/documentation/renderers/openapi/base.rb
+++ lib/spec_forge/documentation/renderers/openapi/base.rb
@@ -42,77 +42,17 @@ module SpecForge
             version = self.class.to_sem_version.morph { |v| "#{v.major}.#{v.minor}" }
 
             paths = Dir[path].map do |path|
+              path = Pathname.new(path)
+
+              label = path.relative_path_from(SpecForge.openapi_path)
               hash = YAML.safe_load_file(path, symbolize_names: true)
 
               hash.transform_values! do |value|
-                Normalizer.normalize!(value, using: "openapi/v#{version}/path")
+                Normalizer.normalize!(value, using: "openapi/v#{version}/path_item", label:)
               end
             end
 
             paths.to_merged_h
-          end
-
-          #
-          # Converts a type string to an OpenAPI schema object
-          #
-          # @param format [String] Type format string
-          #
-          # @return [Hash] OpenAPI schema definition
-          #
-          # @api private
-          #
-          def type_to_schema(format)
-            case format
-            when "datetime", "time"
-              {type: "string", format: "date-time"}
-            when "int64", "i64"
-              {type: "integer", format: "int64"}
-            when "int32", "i32"
-              {type: "integer", format: "int32"}
-            when "double", "float"
-              {type: "number", format:}
-            when "object"
-              {type: "object"}
-            when "array"
-              {type: "array"}
-            when "boolean", "number", "integer", "string"
-              {type: format}
-            else
-              {type: "string", format:}
-            end
-          end
-
-          #
-          # Converts content to an OpenAPI schema object
-          #
-          # @param content [Hash, Array] The content to convert
-          #
-          # @return [Hash] OpenAPI schema properties or items
-          #
-          # @api private
-          #
-          def content_to_schema(content)
-            case content
-            when Hash
-              {properties: content}
-            when Array
-              {items: content}
-            else
-              {}
-            end
-          end
-
-          #
-          # Converts a string to camelCase
-          #
-          # @param string [String] The string to convert
-          #
-          # @return [String] The camelCase version of the string
-          #
-          # @api private
-          #
-          def camelize(string)
-            string.parameterize.underscore.camelcase(:lower)
           end
         end
       end
