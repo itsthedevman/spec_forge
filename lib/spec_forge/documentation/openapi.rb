235276aac25dd73965db21b339b71f8098fef9a3
Bryan <bryan@itsthedevman.com>
2025-05-26 22:07:10 -0400

    Added base and example

--- lib/spec_forge/documentation/openapi.rb
+++ lib/spec_forge/documentation/openapi.rb
@@ -7,6 +7,9 @@ module SpecForge
   end
 end
 
+require_relative "openapi/base"
+
+require_relative "openapi/v3_0/example"
 require_relative "openapi/v3_0/media_type"
 require_relative "openapi/v3_0/operation"
 require_relative "openapi/v3_0/response"
