408f97f32a87f80beda6f2b6c8d83e901b2f995f
Bryan <bryan@itsthedevman.com>
2025-11-17 14:36:04 -0500

    chore(deps): Updated deps and ruby

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    nixpkgs-ruby.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -11,19 +13,18 @@
       self,
       nixpkgs,
       flake-utils,
+      nixpkgs-ruby,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        ruby = nixpkgs-ruby.packages.${system}."ruby-3.2.9";
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (ruby_3_2.override {
-              jemallocSupport = false;
-              docSupport = false;
-            })
+            ruby
 
             # Dependencies for native gems
             pkg-config
