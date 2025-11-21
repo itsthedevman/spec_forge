5c956eee05d20616be1996180fde5cb999a00749
Bryan <bryan@itsthedevman.com>
2025-11-21 16:03:37 -0500

    moved filter

---
+++ lib/spec_forge/loader/filter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module SpecForge
+  class Loader
+    class Filter
+      class << self
+        def announce
+        end
+
+        def apply
+        end
+      end
+    end
+  end
+end
