6da9e394a455a30823446b460aa52d62f0996727
Bryan <bryan@itsthedevman.com>
2026-01-21 22:38:10 -0500

    Fixed args

--- lib/spec_forge/cli/run.rb
+++ lib/spec_forge/cli/run.rb
@@ -70,12 +70,12 @@ module SpecForge
       # @return [void]
       #
       def call
-        path = determine_path
+        base_path = determine_path
         tags = parse_tags(options.tags)
         skip_tags = parse_tags(options.skip_tags)
         verbosity_level = determine_verbosity_level
 
-        blueprints, forge_hooks = Loader.load_blueprints(path:, tags:, skip_tags:)
+        blueprints, forge_hooks = Loader.load_blueprints(base_path:, tags:, skip_tags:)
 
         if blueprints.empty?
           puts "No blueprints found matching the criteria."
